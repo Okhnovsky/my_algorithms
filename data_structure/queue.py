909adb8da3935dc2cca913f7602053d4ff463b47
Alexander <aohnovskiy@swgroup.ru>
2025-09-18 17:53:39 +0300

    fix

--- data_structure/queue.py
+++ data_structure/queue.py
@@ -25,3 +25,23 @@ class Queue:
     def size(self):
         """Вернуть размер очереди."""
         return len(self.__items)
+
+
+from collections import deque
+queue = deque()
+queue.appendleft('item')
+item = queue.pop()  
+
+
+# Потоко-безопасная очередь
+from queue import Queue
+q = Queue(maxsize=0) # size = 0 означает неограниченную длину
+q.put("item") # помещает элемент в конец очереди
+item = q.get() 
+
+
+# Реализация очереди, специально разработанная для межпроцессного взаимодействия.
+from multiprocessing import Queue
+mq = Queue()
+mq.put("item")
+item = mq.get()  
